9b29124854bba6d9566ab191e3d5ffc882b733fa
Ravindar Thirugnanasambandam <ravindar.t.j2ee@gmail.com>
2020-05-20 15:47:03 -0400

    Consumer commit

---
+++ src/main/java/com/example/kafcaConsumer/KafcaConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafcaConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafcaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafcaConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kafcaConsumer/KafcaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafcaConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafcaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
