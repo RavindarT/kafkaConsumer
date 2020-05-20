47245cb1b22549e85636b18862f2a8296287a891
Ravindar Thirugnanasambandam <ravindar.t.j2ee@gmail.com>
2020-05-20 15:49:20 -0400

    commit

--- src/main/java/com/example/kafkaConsumer/KafcaConsumerApplication.java
+++ src/main/java/com/example/kafkaConsumer/KafcaConsumerApplication.java
@@ -1,4 +1,4 @@
-package com.example.kafcaConsumer;
+package com.example.kafkaConsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
